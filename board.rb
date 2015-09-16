680cb35953a2d9be854d1d3666400905c48c0a56
Philippe Hardardt <phil.henri@gmail.com>
2015-09-16 18:46:57 -0300

    White and Black count fields added with fake value to match test

--- board.rb
+++ board.rb
@@ -1,8 +1,10 @@
 class Board
 
-	attr_accessor :piece_count
+	attr_accessor :piece_count, :white_count, :black_count
 
 	def initialize
 		@piece_count = 32
+		@black_count = 16
+		@white_count = 16
 	end
 end
