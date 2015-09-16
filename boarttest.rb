342005f72bd5ece81c91f23efe1a2ea4ed3fff5f
Philippe Hardardt <phil.henri@gmail.com>
2015-09-16 18:45:45 -0300

    New assertion expecting white_count and black_count attributes from board

--- boarttest.rb
+++ boarttest.rb
@@ -6,6 +6,8 @@ class BoardTest < MiniTest::Test
 	def test_board_initialization
 		board = Board.new
 		assert_equal 32, board.piece_count
+		assert_equal 16, board.white_count
+		assert_equal 16, board.black_count
 	end
 
 end
