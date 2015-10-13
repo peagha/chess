8ca42ceb0401e6892c83d0676cead09c821d25dd
Philippe Hardardt <phil.henri@gmail.com>
2015-10-13 17:05:17 -0300

    Rendering of row modified to check if square is occupied to correctly render it

--- boardrender.rb
+++ boardrender.rb
@@ -1,16 +1,21 @@
 class BoardRender
 	def initialize board
+		@board = board
 	end
 
 	def render_rank rank
-		output = ""
-		Board::FILE_RANGE.each do
-			output += "|   "
+		output = "|"
+		Board::FILE_RANGE.each do |file|
+			square = file + rank
+			render = self.class.render_piece(@board[square])
+			output += render + " |"
 		end
-		output += "|"
+		output
 	end
 
 	def self.render_piece piece
+		return "  " if piece.nil?
+
 		piece.team == :black ? 
 			"*" + piece.piece_char :
 			piece.piece_char
