ba1cc2d4d64e4ce872e2b31e5c392ee214a32b8e
Philippe Hardardt <phil.henri@gmail.com>
2015-10-13 16:18:54 -0300

    Test modified to check every piece

--- boardrender_test.rb
+++ boardrender_test.rb
@@ -4,7 +4,18 @@ require_relative 'boardrender'
 
 class BoardRenderTest < MiniTest::Test
 	def test_render_piece
-		assert_equal "K", BoardRender.render_piece(King.white)
+		piece_render = { 
+			King => ?K,
+			Queen => ?Q,
+			Horse => ?N,
+			Bishop => ?B,
+			Rook => ?R,
+			Pawn => ?P }
+		piece_render.each do |piece, render|
+			result = BoardRender.render_piece(piece.white)
+			assert_equal render, result
+		end
+
 		assert_equal "*K", BoardRender.render_piece(King.black)
 	end
 end
