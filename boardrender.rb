a77c44082b74a56d1e8732004fb7e300a464ae2a
Philippe Hardardt <phil.henri@gmail.com>
2015-10-13 16:20:31 -0300

    Piece render modified with every piece char to match test

--- boardrender.rb
+++ boardrender.rb
@@ -1,6 +1,14 @@
 class BoardRender
 	def self.render_piece piece
-		piece_char = ?K
+		piece_render = { 
+			King => ?K,
+			Queen => ?Q,
+			Horse => ?N,
+			Bishop => ?B,
+			Rook => ?R,
+			Pawn => ?P }
+		piece_char = piece_render[piece.class]
+
 		piece.team == :black ? 
 			"*" + piece_char :
 			piece_char
