eb4cdc9ac748c5fdcb1f3b34c82c6a53a4eabdf6
Philippe Hardardt <phil.henri@gmail.com>
2015-10-13 16:43:47 -0300

    New test to check render output of an empty rank

--- boardrender_test.rb
+++ boardrender_test.rb
@@ -16,4 +16,11 @@ class BoardRenderTest < MiniTest::Test
 			assert_equal ?* + piece_char, BoardRender.render_piece(piece.black)
 		end
 	end
+
+	def test_render_empty_rank
+		expected_row = "|   |   |   |   |   |   |   |   |"
+		board = Board.empty
+		renderer = BoardRender.new board
+		assert_equal expected_row, renderer.render_rank(?1)
+	end
 end
