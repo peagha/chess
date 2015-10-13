0fa0a6daf653a1647b7ca3c65be6d0e170e5b4a0
Philippe Hardardt <phil.henri@gmail.com>
2015-10-13 15:59:18 -0300

    New test to check rendering of a black piece

--- boardrender_test.rb
+++ boardrender_test.rb
@@ -5,5 +5,6 @@ require_relative 'boardrender'
 class BoardRenderTest < MiniTest::Test
 	def test_render_piece
 		assert_equal "K", BoardRender.render_piece(King.white)
+		assert_equal "*K", BoardRender.render_piece(King.black)
 	end
 end
