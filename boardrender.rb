68a3de57600f00d51ec221e36a3bc4b9cbc33ada
Philippe Hardardt <phil.henri@gmail.com>
2015-10-13 16:44:10 -0300

    BoardRender modified to return empty rank matching test

--- boardrender.rb
+++ boardrender.rb
@@ -1,4 +1,11 @@
 class BoardRender
+	def initialize board
+	end
+
+	def render_rank rank
+		"|   |   |   |   |   |   |   |   |"
+	end
+
 	def self.render_piece piece
 		piece.team == :black ? 
 			"*" + piece.piece_char :
