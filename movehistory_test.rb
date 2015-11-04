5a451b1e27e58a54b6570eac5035375376122da2
Philippe Hardardt <phil.henri@gmail.com>
2015-11-04 18:06:09 -0200

    Duplicated initialization code moved to test setup

--- movehistory_test.rb
+++ movehistory_test.rb
@@ -2,16 +2,18 @@ require 'minitest/autorun'
 require_relative 'board'
 
 class MoveHistoryTest < MiniTest::Test
+	def setup
+		@board = Board.new.setup
+	end
+
 	def test_one_move_history
-		board = Board.new.setup
-		board.move "a2", "a3"
-		assert_equal [["a3"]], board.move_history
+		@board.move "a2", "a3"
+		assert_equal [["a3"]], @board.move_history
 	end
 
 	def test_two_moves_history
-		board = Board.new.setup
-		board.move "a2", "a3"
-		board.move "a7", "a6"
-		assert_equal [["a3","a6"]], board.move_history
+		@board.move "a2", "a3"
+		@board.move "a7", "a6"
+		assert_equal [["a3","a6"]], @board.move_history
 	end
 end
