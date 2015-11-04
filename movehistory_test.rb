95b66e417bef49134384b4c0501e8634dd3ef79f
Philippe Hardardt <phil.henri@gmail.com>
2015-11-04 17:56:52 -0200

    New test to check move history with more than one movement

--- movehistory_test.rb
+++ movehistory_test.rb
@@ -7,4 +7,11 @@ class MoveHistoryTest < MiniTest::Test
 		board.move "a2", "a3"
 		assert_equal [["a3"]], board.move_history
 	end
+
+	def test_two_moves_history
+		board = Board.new.setup
+		board.move "a2", "a3"
+		board.move "a7", "a6"
+		assert_equal [["a3","a6"]], board.move_history
+	end
 end
