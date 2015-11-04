2fb301e5b27645c3e28e0e1f13976ef39ece857b
Philippe Hardardt <phil.henri@gmail.com>
2015-11-04 17:47:00 -0200

    Test name modified

--- movehistory_test.rb
+++ movehistory_test.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 require_relative 'board'
 
 class MoveHistoryTest < MiniTest::Test
-	def test_pawn_move_history
+	def test_one_move_history
 		board = Board.new.setup
 		board.move "a2", "a3"
 		assert_equal [["a3"]], board.move_history
